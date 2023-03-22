718594bfc3e846e29b87653f54298881a80cd278
SteveBanks0303 <111028784+SteveBanks0303@users.noreply.github.com>
2023-03-22 12:16:16 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, String
+import models
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
-    if storage_type == 'db':
+    if models.is_type == 'db':
+        __tablename__ = 'reviews'
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
